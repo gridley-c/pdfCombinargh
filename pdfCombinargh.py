2cdff3b92ffe4f1839c8d5fcb6b874c3139ad430
gridley-c <crgridley@gmail.com>
2020-06-10 16:41:41 +0100

    completed, with test files

--- pdfCombinargh.py
+++ pdfCombinargh.py
@@ -1,2 +1,29 @@
 #! python3
+# pdfCombinargh.py combines all the selected PDFs in the current working directory
+
+import PyPDF2, os
+
+#get filenames
+pdfFiles = []
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+pdfFiles.sort(key = str.lower)
+
+pdfWriter = PyPDF2.PdfFileWriter()
+
+# loop thru files
+for filename in pdfFiles:
+    pdfFileObj = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    # loop thru all pages except first and add them
+    for pageNum in range(1, pdfReader.numPages):
+        pageObj = pdfReader.getPage(pageNum)
+        pdfWriter.addPage(pageObj)
+
+# save final pdf to a file
+pdfOutput = open('allminutes.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
+
 
